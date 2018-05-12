e7be9b6a5ad5028dc80bcab18ed7c8f9b07986d1
ifriedman08 <i.friedman.08@gmail.com>
2018-05-11 21:17:51 -0700

    added package.json and index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * [description]
+ * @param  {[type]} arg1 [description]
+ * @param  {[type]} x    [description]
+ * @return {[type]}      [description]
+ */
+var starter = function (arg1, x {
+ var f = 4;
+
+};;
